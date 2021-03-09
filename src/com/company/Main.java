32ef0f72fca22293ee92ed0e0b8aff755225028c
dav-oss <60092325+dav-oss@users.noreply.github.com>
2021-03-09 05:38:12 +0000

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,13 @@
 package com.company;
 import java.util.LinkedList;
 import java.util.Scanner;
+import java.util.Iterator;
 
 public class Main {
     static LinkedList<String> girls = new LinkedList<>();
 //Adding a node
     public static void addNode(String girl){
-        while (girl != " "){
+        while (girl.equals(" ")){
             girls.add(girl);
         }
     }
@@ -14,6 +15,20 @@ public class Main {
     public static void removeNode(int option){
         while (option <= girls.size() && option > 0){
                 girls.remove(option);
+        }
+    }
+    //Traversing through the linked list
+
+    /***
+     * At first I didn't know what traversal method is or what is trervasing
+     * I researched and found out that Traversing is just Going through all the items available in an array or a queue
+     * I think am right may be if you may proof me wrong I may agree
+     */
+    public static void traversal(){
+        Iterator treverse = girls.iterator();
+        System.out.println("Present girls already in the queue are: ");
+        while(treverse.hasNext()) {
+            System.out.println(treverse.next());
         }
     }
     public static void main(String[] args) {
@@ -28,22 +43,31 @@ public class Main {
         System.out.println("Would you want to perform OPERATIONS*******????\nenter 1 for (yes)");
         //Creating a Scanner input Object to enter option
         Scanner userInput = new Scanner(System.in);
-        int quiz = userInput.nextInt();
-        //Evaluating the option entered by the user
-        if(quiz == '1'){
-            System.out.println("Please choose an operation to perform\n1. Add a girl to the queue\n2. Remove a girl from the queue");
-            int selection = 0;
-            //Using switch statement to perform the selected option
-            switch(selection){
-                //Adding a girl to the list
-                case 1 : System.out.println("Enter the name of the girl"); String name = userInput.next(); addNode(name);System.out.println("Available girls are : " +girls);break;
-                //Removing a girl from the list
-                case 2 : System.out.println("Enter the number the Girl appears to remove"); int num = userInput.nextInt(); removeNode(num);System.out.println("Available girls are : "+girls);break;
+        int option = 1;
+        while (option ==  1){
+            int quiz = userInput.nextInt();
+            //Evaluating the option entered by the user
+            if(quiz == '1'){
+                System.out.println("Please choose an operation to perform\n1. Add a girl to the queue\n2. Remove a girl from the queue\n3. View available girls in the queue");
+                int selection = 0;
+                //Using switch statement to perform the selected option
+                switch(selection){
+                    //Adding a girl to the list
+                    case 1 : System.out.println("Enter the name of the girl"); String name = userInput.next(); addNode(name);traversal();break;
+                    //Removing a girl from the list
+                    case 2 : System.out.println("Enter the number the Girl appears to remove"); int num = userInput.nextInt(); removeNode(num);traversal();break;
+                    //Displaying available names in the queue
+                    case 3 : traversal();break;
+                }
+            }//Display text message and terminate the program
+            else{
+                System.out.println("Thank you for checking available girls,\nREMEMBER TO COME BACK WE MAY HAVE MORE###");
+                System.exit(0);
             }
-        }//Display text message and terminate the program
-        else{
-            System.out.println("Thank you for checking available girls,\nREMEMBER TO COME BACK WE MAY HAVE MORE###");
-            System.exit(0);
+
+            //Querrying the user if he/she wants to continue
+            System.out.println("Would you want to continue ?\n\t1. Yes\n\t2. No");
+            option = userInput.nextInt();
         }
     }
 }
